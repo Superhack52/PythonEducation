0083d02c873bde8f243a8b2f2898e4627d1fa19a
Owner <krasilse@gmail.com>
2018-08-07 19:59:58 +0300

    victorina and simple object

---
+++ simple_critter.py
@@ -0,0 +1,8 @@
+# Простой класс и объект
+class Critter(object):
+    """Виртуальный питомец"""
+    def talk(self):
+        print("Привет. Я зверюшка - экземпляр класса Critter.")
+crit = Critter()
+crit.talk()
+input("\n\nНажмите Enter, чтовы выйти.")
