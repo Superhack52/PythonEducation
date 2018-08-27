134d4d6878a692b6fde9091eb36153ebfc0fa591
Unknown <krasilse@gmail.com>
2018-08-27 20:44:59 +0300

    add check to get method

--- cheast_quest.py
+++ cheast_quest.py
@@ -1,6 +1,7 @@
 class ChestFactory(object):
     __GOODS = ["Щит","Меч", "Булава", "Кирка","Броня" ,"И так далее"]
     class Chest(object):
+        __chestIsEmpty = "Сундук пуст!"
         def __init__(self, thing):
                 self.__thing = thing
                 self.__isOpened = False
@@ -10,16 +11,19 @@ class ChestFactory(object):
                 if not self.__isEmpty:
                     print("Сундук уже открыт. В нем лежит -", self.__thing)
                 else:
-                    print("Сундук пуст!")
+                    print(self.__chestIsEmpty)
             else:
                 self.__isOpened = True
                 print("Сундук открывается и в нем лежит -", self.__thing)
         def Get(self):
             content = self.__thing
-            print("Вы забираете", content, "из сундука.")
-            self.__thing = None
-            self.__isEmpty = True
-            return content
+            if content:
+                print("Вы забираете", content, "из сундука.")
+                self.__thing = None
+                self.__isEmpty = True
+                return content
+            else:
+                print(self.__chestIsEmpty)
         def __str__(self):
             return "Сундук. В нем лежит" + " " + self.__thing      
     @staticmethod
@@ -31,4 +35,5 @@ chestOne = ChestFactory.GetChest()
 chestOne.Open()
 goodFromChest = chestOne.Get()
 chestOne.Open()
+chestOne.Get()
 input("\nНажмите Enter.")
